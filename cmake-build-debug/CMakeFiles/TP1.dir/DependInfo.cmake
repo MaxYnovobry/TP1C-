a15df53113f8890dcc7c267813a2e61509e71bfc
unknown <laurie.jeham@hotmail.fr>
2022-04-01 14:01:56 +0200

    ordi maison

---
+++ cmake-build-debug/CMakeFiles/TP1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/CLionProjects/TP1C-/Source/Exercice1.cpp" "CMakeFiles/TP1.dir/Source/Exercice1.cpp.obj" "gcc" "CMakeFiles/TP1.dir/Source/Exercice1.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/TP1C-/Source/Exercice2.cpp" "CMakeFiles/TP1.dir/Source/Exercice2.cpp.obj" "gcc" "CMakeFiles/TP1.dir/Source/Exercice2.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/TP1C-/Source/Exercice3.cpp" "CMakeFiles/TP1.dir/Source/Exercice3.cpp.obj" "gcc" "CMakeFiles/TP1.dir/Source/Exercice3.cpp.obj.d"
+  "C:/Users/Admin/CLionProjects/TP1C-/main.cpp" "CMakeFiles/TP1.dir/main.cpp.obj" "gcc" "CMakeFiles/TP1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
